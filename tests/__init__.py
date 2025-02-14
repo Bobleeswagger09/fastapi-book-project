5e949c0c7b694fa3f67c49a126cb590d87dd4e87
Ubuntu <ubuntu@ip-172-31-26-198.eu-north-1.compute.internal>
2025-02-14 12:07:19 +0000

    f

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,4 @@
 from fastapi.testclient import TestClient
-<<<<<<< HEAD
-=======
-
->>>>>>> 1bad0e1c27fbb4bca60ceb33189d127d8eaabc4f
 from main import app
 
 client = TestClient(app, base_url="http://test/api/v1")
